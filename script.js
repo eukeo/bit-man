e937b7904151dcc42e0be7bcbb5f40cde7538f43
eukeo <sixsblooms@gmail.com>
2022-05-24 16:32:04 -0700

    final-push

---
+++ script.js
@@ -0,0 +1,45 @@
+let guesses = 5;
+let winGuesses = 6;
+let myWord = ["PIXELS"];
+let wordArray = ('PIXELS').split("");
+let prompt = document.querySelector("p");
+
+// Button and Letter function
+let buttons = document.getElementById("letters")
+  console.log(buttons);
+    buttons.addEventListener('click', eventClick)
+function eventClick(event){
+  console.log(event.target.textContent);
+
+// Correct and Incorrect     
+if (wordArray.includes(event.target.textContent)){
+let divId = wordArray.indexOf(event.target.textContent)
+  let divLetter = document.getElementById(`${divId}`)
+    divLetter.textContent = event.target.textContent
+    console.log("Correct!");
+      event.target.style.backgroundColor = '#35ffa2'
+      event.target.disabled = true;
+        prompt.innerHTML = 'Correct!'
+          winGuesses -= 1;
+          console.log(winGuesses)
+  } else {
+    guesses -= 1;
+    console.log("Incorrect!");
+      event.target.style.backgroundColor = '#0091cd';
+      event.target.disabled = true;
+        prompt.innerHTML = 'Incorrect!'
+          counter.innerHTML = (guesses + ' lives left!')
+          console.log(guesses + ' lives left!');
+
+// Win/Lose Function
+  } if (guesses === 0){
+      prompt.innerHTML = 'You Lose...'
+      buttons.removeEventListener('click', eventClick)
+        console.log("You Lose...")
+  } if (winGuesses === 0){
+      console.log(winGuesses)
+      prompt.innerHTML = 'You Win!'
+      buttons.removeEventListener('click', eventClick)
+        console.log("You Win!")
+    }
+  };
